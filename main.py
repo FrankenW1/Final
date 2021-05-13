986f561a9c7c51b67bfbafd0b02a65a0e901e6e9
Will Franken <FrankenW@jbu.edu>
2021-05-13 07:48:16 -0500

    Working on Final Project

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ state_abb = ["AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "
               "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH",
               "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX",
               "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
-@tax_router.get("/calculate")
+@tax_router.get("/calculate_tax")
 def calc_tax(state: str, income: int or float):
 
     for item in list_states:
@@ -36,6 +36,14 @@ def calc_tax(state: str, income: int or float):
             return {"remaining": float("{:.2f}".format(total_inc)),
                     "tax": float("{:.2f}".format(total_tax)),
                     "state":f"{state}"}
+
+@tax_router.get("/calculate_asset")
+def calc_asset(invested: int or float):
+    amount_return_month = invested * (0.1/12)
+    amount_return_year = invested * 0.1
+    return {"amount invested": float("{:.2f}".format(invested)),
+            "return per month": float("{:.2f}".format(amount_return_month)),
+            "return per year": float("{:.2f}".format(amount_return_year))}
 app.include_router(tax_router)
 if __name__ == "__main__":
     uvicorn.run("serv:app", host="0.0.0.0", reload="true")
